0d55e436b1090c13969a761366ecf275924346a8
Neil Toledo <ntoledo@berkeley.edu>
2021-03-29 10:12:13 -0700

    coordinator in working state

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import generateGateway from './mmo-gateway/dist/app.js'
+import Logger from './mmo-gateway/src/loaders/logger';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+async function startServer(port) {
+	const app = express()
+	const gatewayApp = await generateGateway();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+	app.use("/", gatewayApp);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+	const server = app.listen(port);
+	server.on('error', onError);
+	server.on('listening', () => onListening(server));
+}
 
-module.exports = app;
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      Logger.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      Logger.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening(server) {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  Logger.debug('Listening on ' + bind);
+}
+
+startServer(3000)
