7a8e6ac96888f2798f07ebc848438a77997dc104
Neil Toledo <ntoledo@berkeley.edu>
2021-03-30 03:15:55 -0700

    removed babel from coordinator

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-import express from 'express';
-import generateGateway from './mmo-gateway/dist/app.js'
-import Logger from './mmo-gateway/src/loaders/logger';
-
+var express = require('express');
+var generateGateway = require('./mmo-gateway/dist/app.js');
 
 async function startServer(port) {
 	const app = express()
@@ -26,11 +24,11 @@ function onError(error) {
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      Logger.error(bind + ' requires elevated privileges');
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      Logger.error(bind + ' is already in use');
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -43,7 +41,7 @@ function onListening(server) {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  Logger.debug('Listening on ' + bind);
+  console.debug('Listening on ' + bind);
 }
 
 startServer(3000)
